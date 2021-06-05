5b51a7dd119c2a86bc3af1c33cc4525bd4ddef76
rohw <rohw@engr.oregonstate.edu>
2021-06-05 17:24:15 +0900

    added test multiply

--- test_calculator.py
+++ test_calculator.py
@@ -10,3 +10,7 @@ class TestCalculatorApp:
 
     def test_subtract(self):
         assert 5 == calculator.subtract(10,5)
+    
+    def test_multiply(self):
+        assert 50 == calculator.multiply(10,5)
+        
